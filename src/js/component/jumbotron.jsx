46747ac6550d6ba7a2cd7abb63d1b7b469c6bf71
smit shah <shahsmit293@gmail.com>
2023-08-24 22:45:18 +0000

    Final Changes from smit

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
 const Jumbotron = () => {
-  
-    return (
-        <div class="container bg-light p-5 border border-secondary">
-  <h1 class="display-4">A warm welcome</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information. sdfdf sd sd ksd icj ocdij socj sdcd jska ask mdsk s</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
-  </p>
-</div>
-    )
-}
+  return (
+    <div className="container p-5">
+      <h1 className="display-4">A Warm Welcome!</h1>
+      <p className="lead">
+        This is a simple hero unit, a simple jumbotron-style component for
+        calling extra attention to featured content or information. sdfdf sd sd
+        ksd icj ocdij socj sdcd jska ask mdsk s
+      </p>
+      <p className="lead">
+        <a className="btn btn-primary btn-lg" href="#" role="button">
+          Call to action!
+        </a>
+      </p>
+    </div>
+  );
+};
 
-export default Jumbotron; 
+export default Jumbotron;
